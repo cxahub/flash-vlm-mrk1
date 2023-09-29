dcd5f3d08d009a0d7e5b28527d805eb932f23cbf
cxahub <chad.fraser@sap.com>
2023-09-29 09:44:38 -0600

    Broken

--- pages/report/index.vue
+++ pages/report/index.vue
@@ -1,10 +1,34 @@
 <template>
-  <div v-if="complete < 100">
-    <h1
-      class="text-3xl xl:text-5xl text-center xl:text-left uppercase font-bold py-4"
+  <h1 class="text-3xl xl:text-5xl uppercase font-bold py-4">Thank You</h1>
+  <div class="text-lg">
+    Your <span class="font-bold">FLASH&trade;</span> Report is ready for
+    download!<br /><br />
+    <UiButton
+      class="py-4"
+      text="Download"
+      path="/report"
+      format="primary"
+      size="lg"
+    /><br />
+    If you are unable to download your report please contact us at:
+    <NuxtLink
+      :to="`mailto:flash@sap.com?subject=FLASH Download Problem - ${company}`"
+      activeClass="text-fs-yellow"
+      class="underline"
+      target="_blank"
+      >flash@sap.com</NuxtLink
+    ><br /><br />
+
+    Please provide feedback regarding your experience, request a meeting, or
+    visit our
+    <NuxtLink
+      :to="firestartersURL"
+      activeClass="text-fs-yellow"
+      class="underline"
+      target="_blank"
     >
       FLASH&trade; Report
-    </h1>
+    </NuxtLink>
     <p class="text-lg text-center xl:text-left">
       You must complete all questions to download your FLASH&trade; report.
     </p>
@@ -67,19 +91,10 @@
 </template>
 
 <script setup>
-import nuxtStorage from "nuxt-storage";
-
-const token = nuxtStorage.localStorage.getData("token");
-
-const showDownload = ref(false);
-
-if (!token) {
-  navigateTo("/profile?timeout=true");
-}
-
 const config = useRuntimeConfig();
 const firestartersURL = config.public.VUE_APP_FIRESTARTERS_WEBSITE_URL;
-const companyName = useCookie("companyName").value;
+
+const company = ref(useCookie("company") || false);
 
 definePageMeta({
   layout: "default",
@@ -93,74 +108,4 @@ definePageMeta({
       },
     ],
   });
-
-function getReport() {
-  getDownload()
-    .then((result) => {
-      showDownload.value = false;
-      const url = window.URL.createObjectURL(
-        new Blob([result], {
-          type: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-        })
-      );
-      const link = document.createElement("a");
-
-      const today = new Date();
-      const yyyy = today.getFullYear();
-      let mm = today.getMonth() + 1; // Months start at 0!
-      let dd = today.getDate();
-
-      if (dd < 10) dd = '0' + dd;
-      if (mm < 10) mm = '0' + mm;
-
-      const formattedToday = dd + '-' + mm + '-' + yyyy;
-      const filename = 'FLASH Enterprise Retail Report - ' + formattedToday + '.pptx';
-
-      link.href = url;
-      link.setAttribute("download", filename);
-      document.body.appendChild(link);
-
-      link.click();
-      link.remove();
-      window.URL.revokeObjectURL(url);
-    })
-    .catch((error) => {
-      console.error("Get report could not be sent", error);
-    });
-}
-
-async function getDownload() {
-  return await $fetch(
-    config.public.VUE_APP_API_URL +
-      "/" +
-      config.public.VUE_APP_API_VLM_REPORT_ROUTE,
-    {
-      method: "GET",
-      query: {
-        "x-auth-token": nuxtStorage.localStorage.getData("token"),
-        surveyId: useCookie("surveyID").value,
-      },
-    }
-  );
-}
-
-const options = {
-  method: "GET",
-  query: {
-    "x-auth-token": nuxtStorage.localStorage.getData("token"),
-    customerSurveyId: useCookie("surveyID").value,
-    completedpercentage: true,
-  },
-};
-
-//Fetch data.
-const { pending, data: complete } = await useLazyFetch(
-  config.public.VUE_APP_API_URL +
-    "/" +
-    config.public.VUE_APP_API_VLM_SURVEY_ROUTE,
-  {
-    method: options.method,
-    query: options.query,
-  }
-);
 </script>
