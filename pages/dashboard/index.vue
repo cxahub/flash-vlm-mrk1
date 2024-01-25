528a5f276968c14304a81513e5943a8fa71931ef
cxahub <chad.fraser@sap.com>
2024-01-25 13:22:54 -0700

    Asessment text fix.

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -2,15 +2,15 @@
   <h1
     class="text-3xl text-center xl:text-left xl:text-5xl font-bold text-fs-yellow drop-shadow-sm"
   >
-    My Assesssments
+    My Assessments
   </h1>
   <div class="grid xl:grid-cols-2 gap-0 xl:gap-4">
     <div class="text-center xl:text-left xl:text-xl py-8">
       <span class="font-bold"
         >We'll keep track of your progress for you on this page.<br
       /></span>
-      Click the "Start Assesssment Now" button to get started with your
-      assessment. You can also choose from the many question topics below.<br /><br />
+      Click the "Start Assesssment Now" button to begin. You can also choose
+      from the many question topics below.<br /><br />
       NOTE: Once you have answered questions, you can update your assessment by
       clicking any of the questions below and making your changes.
     </div>
@@ -19,13 +19,13 @@
         to="/survey/1/1/customer%20centricity"
         class="max-w-max text-black font-bold uppercase rounded-lg cursor-pointer drop-shadow-lg bg-fs-yellow hover:bg-fs-yellow-light text-xl xl:text-2xl py-4 px-4 my-4 xl:py-6 xl:px-6 xl:my-6"
       >
-        Start Assesssment Now &rarr;</NuxtLink
+        Start Assessment Now &rarr;</NuxtLink
       >
     </div>
 
     <div class="py-10 text-center xl:text-left">
       <UiButton
-        :text="!showSurveyList ? 'Show My Assesssments' : 'Back to Dashboard'"
+        :text="!showSurveyList ? 'Show My Assessments' : 'Back to Dashboard'"
         path=""
         @click="showSurveyList = !showSurveyList"
       />
